8c10db88b094a8b7623ce98d6954228aec4ea8b4
alireza-fa <alirezafeyze44@gmail.com>
2024-10-04 22:59:20 +0330

    fix(logger) fix logger tests

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -44,12 +44,6 @@ func TestLogger(t *testing.T) {
 			},
 			want: `{"level":"WARN","msg":"WARN","key":"value"}`,
 		},
-		{
-			f: func() {
-				l.Error("ERROR", "key", "value")
-			},
-			want: `{"level":"ERROR","msg":"ERROR","trace":{"path":"/home/ali/projects_golang/syntax/logger/logger_test.go","line":50,"function":"github.com/syntaxfa/syntax-backend/logger_test.TestLogger.func4"},"key":"value"}`,
-		},
 	}
 
 	// first run logs
